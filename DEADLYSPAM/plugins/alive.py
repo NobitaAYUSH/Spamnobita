33f2863a3b78ed044d1e9414605be2808e823a01
NobitaAYUSH <162026272+NobitaAYUSH@users.noreply.github.com>
2024-04-07 16:40:53 +0530

    Update alive.py

--- DEADLYSPAM/plugins/alive.py
+++ DEADLYSPAM/plugins/alive.py
@@ -8,12 +8,12 @@ PIC = config.ALIVE_PIC
 if config.ALIVE_PIC:
     DEADLY_PIC = PIC
 else:
-    DEADLY_PIC = "https://te.legra.ph/file/2e2f78610814092d61103.jpg"
+    DEADLY_PIC = "https://telegra.ph/file/0d57c17b3ccfd304a935d.jpg"
 
 hl = config.CMD_HNDLR
 
 
-DEADLY = "✯ 𝗞𝗜𝗡𝗚 𝗫 𝗧𝗘𝗔𝗠 𝗕𝗥𝗔𝗡𝗗𝗘𝗗 ✯\n\n"
+DEADLY = "✯ NOBITA_X_SPAM_BOT✯\n\n"
 DEADLY += f"═══════════════════\n"
 DEADLY += f"• **ᴘʏᴛʜᴏɴ ᴠᴇʀsɪᴏɴ** : `4.0.0`\n"
 DEADLY += f"• **ᴛᴇʟᴇᴛʜᴏɴ ᴠᴇʀsɪᴏɴ** : `{version.__version__}`\n"
@@ -24,7 +24,8 @@ DEADLY += f"═══════════════════\n\n"
 @BOT0.on(events.NewMessage(incoming=True, pattern=r"\%salive(?: |$)(.*)" % hl))
 async def alive(event): 
   if event.sender_id in SUDOERS:
-     Blaze = [[Button.url("ᴄʜᴀɴɴᴇʟ", "https://t.me/BRANDED_KHUSHI"), Button.url("sᴜᴘᴘᴏʀᴛ", "https://t.me/BRANDED_WORLD")], [Button.url("• ʀᴇᴘᴏ •", "https://github.com/WCGKING/KINGSPAM")]]
+     Blaze = [[Button.url("ᴄʜᴀɴɴᴇʟ", "https://t.me/NOBITA_DEVLOPE_THINGS"), Button.url("sᴜᴘᴘᴏʀᴛ", "https://t.me/MONSTER_X_SUDO")], 
+              [Button.url("• ʀᴇᴘᴏ •", "https://github.com/WCGKING/KINGSPAM")]]
      await BOT0.send_file(event.chat_id, DEADLY_PIC, caption=DEADLY, buttons=Blaze) 
   else:
-      await event.reply("**ᴅᴇᴘʟᴏʏ ʏᴏᴜʀ ᴏᴡɴ ᴋɪɴɢ x ᴛᴇᴀᴍ ʙʀᴀɴᴅᴇᴅ-ꜱᴘᴀᴍʙᴏᴛ!**") 
+      await event.reply("**!𝐁𝐎𝐒𝐃𝐈𝐊𝐄 𝐉𝐀 𝐏𝐀𝐄 𝐍𝐎𝐁𝐈𝐓𝐀 𝐊𝐀 𝐋𝐄 𝐕𝐎 𝐅𝐈𝐑 𝐒𝐔𝐃𝐎 𝐃𝐄 𝐃𝐄 𝐆𝐀 🥀**") 
